8abc9ee39f40a331b4c68354f3b506165e9ca97c
Artem Kalachev <Artem.Kalachev@Symphonyteleca.com>
2015-11-06 11:52:29 +0300

    Initial Project Commit

---
+++ HDOUTViewer/HDOUTViewer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HDOUTViewer
+//
+//  Created by Artem Kalachev on 06/11/15.
+//  Copyright © 2015 Artem Kalachev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HDOUTViewer/HDOUTViewer/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HDOUTViewer
+//
+//  Created by Artem Kalachev on 06/11/15.
+//  Copyright © 2015 Artem Kalachev. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
